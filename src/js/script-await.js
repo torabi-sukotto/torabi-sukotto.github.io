f504b54df432246825dc1e1f7d28a1ed6c4cde5c
torabi-sukotto <105126180+torabi-sukotto@users.noreply.github.com>
2022-09-14 19:01:34 +0200

    Delete src directory

--- src/js/script-await.js
+++
@@ -1,9 +0,0 @@
-async function addPokemon(nameOrIndex) {
-    try {
-        let req = await fetch(`${ROOT_URL}/pokemon/${nameOrIndex}/`);
-        let data = await req.json();
-        addPokemonCard(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
--- src/js/script-fetch.js
+++
@@ -1,6 +0,0 @@
-function addPokemon(nameOrIndex) {
-    fetch(`${ROOT_URL}/pokemon/${nameOrIndex}`)
-        .then(response => response.json())
-        .then(data => addPokemonCard(data))
-        .catch(error => console.log(error));
-}
